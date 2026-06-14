6c15041199d525cb3d2970f99095fa34e7d5626a
TheGoodDevil00 <hitesh.200711@gmail.com>
2026-06-14 21:34:47 +0530

    chore: whitelist Spotify image domains in next config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+      },
     ],
   },
 };
